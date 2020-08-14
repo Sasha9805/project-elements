f9bb53e2785c005a6130e4b2c459be52b764e460
Sasha9805 <pimenovs33@gmail.com>
2020-08-14 21:36:10 +0300

    Выполнены все 5 заданий по работе с эл-ами на странице

--- js/script.js
+++ js/script.js
@@ -24,3 +24,55 @@ const movieDB = {
     ]
 };
 
+// Первое задание
+const advertisement = document.querySelectorAll('.promo__adv img');
+advertisement.forEach(item => item.remove());
+
+// Второе задание
+const promoBg = document.querySelector('.promo__bg');
+// const genre = document.querySelector('.promo__genre');
+const genre = promoBg.querySelector('.promo__genre');
+genre.textContent = 'драма';
+
+// Третье задание
+// promoBg.style.cssText = `
+//     background: url('../img/bg.jpg') center center/100% 100% no-repeat;
+// `;
+promoBg.style.backgroundImage = "url('../img/bg.jpg')";
+
+// Четвертое и пятое задание
+const listOfFilms = document.querySelector('.promo__interactive-list');
+const itemsOfFilms = listOfFilms.querySelectorAll('.promo__interactive-item');
+const filmsSort = movieDB.movies.sort();
+// listOfFilms.innerHTML = '';
+// Первый способ
+// for (let i = 0; i < filmsSort.length; i++) {
+//     const li = document.createElement('li');
+//     li.classList.add('promo__interactive-item');
+//     li.innerHTML = `${i + 1}. ${filmsSort[i]} <div class="delete"></div>`;
+//     listOfFilms.append(li);
+// }
+// Второй способ
+// filmsSort.forEach((film ,i) => {
+//     listOfFilms.innerHTML += `
+//         <li class="promo__interactive-item">${i + 1}. ${film}
+//             <div class="delete"></div>
+//         </li>
+//     `;
+// });
+// Третий способ
+// Не нужно очищать сам список
+itemsOfFilms.forEach((item, i) => {
+    // Выведет так этот текст, нужно заранее создать эл-т
+    // item.prepend(`<span>${i}</span>`);
+    
+    // item.innerHTML = `${filmsSort[i]}`;
+    // item.insertAdjacentHTML('afterbegin', `${i}. `);
+
+    item.innerHTML = `${i + 1}. ${filmsSort[i]} <div class="delete"></div>`;
+});
+
+// Node List не работает так -> Type Error
+// for (let i = 0; i < itemsOfFilms.length; i++) {
+//     itemsOfFilms[i] = filmsSort[i];
+// }
